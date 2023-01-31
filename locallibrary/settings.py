1d8b38e19759c3da72def3153805c38017a87580
avnrupatunga <115935450+avnrupatunga@users.noreply.github.com>
2023-01-31 16:02:10 +0530

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -132,3 +132,7 @@ LOGIN_REDIRECT_URL = '/'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATIC_URL = '/static/'
